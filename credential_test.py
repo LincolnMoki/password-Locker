16a66d208ffeb9bb4a6175415844c185788b32e2
Ronny <mwithiamoki@gmail.com>
2020-09-07 08:56:56 +0300

    add test find

--- credential_test.py
+++ credential_test.py
@@ -31,13 +31,26 @@ class TestCredential(unittest.TestCase):
             test_credential = Credential("Test1","user","apple","test1@user.com") 
             test_credential.save_credential()
             self.assertEqual(len(Credential.credential_list),2)
-     def test_delete_credential(self):
+
+    def test_delete_credential(self):
             
             self.new_credential.save_credential()
             test_credential = Credential("Test2","user","ball","test2@user.com") 
             test_credential.save_credential()
 
             self.new_credential.delete_credential()
-            self.assertEqual(len(Credential.credential_list),1)         
+            self.assertEqual(len(Credential.credential_list),1) 
+    def test_find_credential_by_credential_name(self):
+       
+
+        self.new_credential.save_credential()
+        test_credential = Credential("Test3","user","curry","test3@user.com") 
+        test_credential.save_credential()
+
+        found_credential = Credential.find_by_name("Test")
+
+        self.assertEqual(found_credential.email,test_credential.email)  
+
+                
 
     
