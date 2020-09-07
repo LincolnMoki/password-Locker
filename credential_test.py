4297987c91db9a56a75ae2d2fdad3f398b73aa25
Ronny <mwithiamoki@gmail.com>
2020-09-07 08:59:47 +0300

    add test exists

--- credential_test.py
+++ credential_test.py
@@ -28,14 +28,14 @@ class TestCredential(unittest.TestCase):
     def test_save_multiple_credential(self):
             
             self.new_credential.save_credential()
-            test_credential = Credential("Test1","user","apple","test1@user.com") 
+            test_credential = Credential("Test","user","dog","test1@user.com") 
             test_credential.save_credential()
             self.assertEqual(len(Credential.credential_list),2)
 
     def test_delete_credential(self):
             
             self.new_credential.save_credential()
-            test_credential = Credential("Test2","user","ball","test2@user.com") 
+            test_credential = Credential("Test","user","dog","test2@user.com") 
             test_credential.save_credential()
 
             self.new_credential.delete_credential()
@@ -44,12 +44,28 @@ class TestCredential(unittest.TestCase):
        
 
         self.new_credential.save_credential()
-        test_credential = Credential("Test3","user","curry","test3@user.com") 
+        test_credential = Credential("Test","user","dog","test3@user.com") 
         test_credential.save_credential()
 
         found_credential = Credential.find_by_name("Test")
 
         self.assertEqual(found_credential.email,test_credential.email)  
+    
+    def test_credential_exists(self):
+        
+
+        self.new_credential.save_credential()
+        test_credential = Credential("Test4","user","dog","test4@user.com") 
+        test_credential.save_credential()
+
+        credential_exists = Credential.credential_exist("dog")
+        self.assertTrue(credential_exists)    
+        
+                       
+
+
+if __name__ == '__main__':
+    unittest.main()        
 
                 
 
