e9d326573eb6b948175fe01ef896dde6e5086370
Ronny <mwithiamoki@gmail.com>
2020-09-07 08:45:36 +0300

    add test save

--- user_test.py
+++ user_test.py
@@ -16,5 +16,17 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.first_name,"Alpha")
         self.assertEqual(self.new_user.last_name,"Rome")
         self.assertEqual(self.new_user.password,"rome231")
-     
+        
+     def test_save_user(self):
+       
+        self.new_user.save_user() 
+        self.assertEqual(len(User.user_list),1)
+        
+     def test_save_multiple_user(self):
+       
+        self.new_user.save_user()
+        test_user = User("Facebook","Alpha","Rome","rome231") 
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+    
                
