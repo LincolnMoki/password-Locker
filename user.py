4e52eeddd780b66c164ed4479f1399c47068cc2c
Ronny <mwithiamoki@gmail.com>
2020-09-07 08:42:38 +0300

    initial commit

---
+++ user.py
@@ -0,0 +1,16 @@
+import pyperclip
+class User:
+   
+    user_list = []
+
+    def __init__(self,first_name,last_name,user_name,password):
+
+    
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
+        
+    user_list = []
+    def save_user(self):
