68154a73f5716bc8009a01e02eb856727635f486
Ronny <mwithiamoki@gmail.com>
2020-09-07 08:46:49 +0300

    add new user save

--- user_test.py
+++ user_test.py
@@ -16,7 +16,7 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.first_name,"Alpha")
         self.assertEqual(self.new_user.last_name,"Rome")
         self.assertEqual(self.new_user.password,"rome231")
-        
+
      def test_save_user(self):
        
         self.new_user.save_user() 
@@ -28,5 +28,27 @@ class TestUser(unittest.TestCase):
         test_user = User("Facebook","Alpha","Rome","rome231") 
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
+      def tearDown(self):
+         
+         User.user_list = []
+
+     def test_delete_user(self):
+        
+         self.new_user.save_user()
+         test_user = User("Facebook","Alpha","Rome","rome231") 
+         test_user.save_user()
+
+         self.new_user.delete_user()
+         self.assertEqual(len(User.user_list),1)
+     def test_find_user_by_username(self):
+         
+
+         self.new_user.save_user()
+         test_user = User("Facebook","Alpha","Rome","rome231")
+         test_user.save_user()
+
+         found_user = User.find_by_user_name("Rome")
+
+         self.assertEqual(found_user.user_name,test_user.user_name)   
     
                
