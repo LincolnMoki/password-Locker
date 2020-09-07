ed640d276cc5889011370161864de7898fad1451
Ronny <mwithiamoki@gmail.com>
2020-09-07 08:54:03 +0300

    add tear down and save

--- credential_test.py
+++ credential_test.py
@@ -14,3 +14,22 @@ class TestCredential(unittest.TestCase):
         self.assertEqual(self.new_credential.usr_name,"Rome")
         self.assertEqual(self.new_credential.password,"231")
         self.assertEqual(self.new_credential.email,"Aplharome@gmail.com")
+     def test_save_credential(self):
+       
+        self.new_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list),1)  
+
+
+    def tearDown(self):
+            
+            Credential.credential_list = []    
+
+
+    def test_save_multiple_credential(self):
+            
+            self.new_credential.save_credential()
+            test_credential = Credential("Test1","user","apple","test1@user.com") 
+            test_credential.save_credential()
+            self.assertEqual(len(Credential.credential_list),2)
+
+    
