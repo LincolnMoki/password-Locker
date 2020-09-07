53a444b858a5316aab04c9c8eded654eeb0df2ed
Ronny <mwithiamoki@gmail.com>
2020-09-07 08:43:30 +0300

    add class method

--- user.py
+++ user.py
@@ -14,3 +14,34 @@ class User:
         
     user_list = []
     def save_user(self):
+     User.user_list.append(self)
+    def delete_user(self):
+    
+       
+
+        User.user_list.remove(self)
+    @classmethod
+    def find_by_user_name(cls,user_name):
+       
+
+        for user in cls.user_list:
+            if user.user_name == user_name:
+                return user_name
+    @classmethod
+    def user_exist(cls,user_name):
+       
+        for user in cls.user_list:
+            if user.user_name == user_name:
+                    return True
+
+        return False
+    @classmethod
+    def display_user_name(cls):
+       
+        return cls.user_list
+    
+    @classmethod
+    def copy_password(cls,password):
+        user_found = User.find_by_password(password)
+        pyperclip.copy(user_found.password)
+    
